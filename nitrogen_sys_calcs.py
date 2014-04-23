042f46c2f7a7ff002b8ea04644104e18156cdf89
powersjcb <powersjcb@gmail.com>
2014-04-23 07:10:49 -0700

    Adding flow calculations

--- nitrogen_sys_calcs.py
+++ nitrogen_sys_calcs.py
@@ -12,6 +12,14 @@ T_operation = 100 	# degrees K, (-135 deg C)
 X_max = 0.01		# Max allowable quality NEEDS TO BE VERIFIED BY EXPERIENCE OF DECAM
 
 
+#Defined Mass Flow Data
+
+m_dot = 300			# grams/second
+D = .0334-.00377*2
+print D			# meter, inside diameter of tubing
+
+
+
 #Derived Heat Loads
 
 L_pipe = 80			# Meters of vacuum insulated hose
@@ -37,17 +45,23 @@ print "Q_net = ", '%.1f' % Q_net
 cam_height = 18.6	# meters, height difference from the utility room to the camera high spot
 g = 9.81 			# m/s^2, gravity
 
-#Density of Nitrogen at 100K as a liquid.
+#Density of Nitrogen at T_operation as a liquid.
 rho = CP.Props('D','T', T_operation, 'Q', 0,"Nitrogen") 
 
 dP_head = rho*g*cam_height/1000	# kPa, static pressure head from util room to camera
 
+#Computing dynamics losses
+
+# velocity = 
+
+# Re = 
 
 
-
-
-# dP_flow_hose   = 
-# dP_flow_smooth = 
+# def dP():
+# 	dP = f_D*L/D*rho*V
+# 	return dP
+# # dP_flow_hose   = 
+# # dP_flow_smooth = 
 
 print "dP_head = " '%.1f' % dP_head
 
